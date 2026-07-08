4d9ff00960a920dbfb3cb4b63afa1e3cf910fad7
prashanth837 <prashanthkurimisetty@gamil.com>
2026-07-08 20:37:51 +0530

    Initial House Hunt project

---
+++ frontend/src/services/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+// Create axios instance
+const api = axios.create({
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor to automatically attach authorization header
+api.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user && user.token) {
+      config.headers['Authorization'] = `Bearer ${user.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor to handle token expiration/401 errors
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Token has failed, expired or is invalid
+      localStorage.removeItem('user');
+      // We can force reload or dispatch a logout event
+      if (!window.location.pathname.includes('/login')) {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
